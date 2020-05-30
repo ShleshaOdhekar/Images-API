53f074cfa9f583bd5ab6dc0ec3a1f93125711749
Shlesha <2018.shlesha.odhekar@ves.ac.inclean>
2020-05-30 20:39:54 +0530

    change url

--- backend_app/urls.py
+++ backend_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
     # anything going to /api gets redirected to the django rest API stuff
-    path('api/', include('imageupload_rest.urls')),
+    path('', include('imageupload_rest.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # we are serving static and media files here at the moment - if we deploy this app to a server, we do necessarily want this
